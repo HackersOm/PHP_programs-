038f23cd76ea1d322d86fb77839cd8a774dbc9ef
hacker <hackeromsutar@gmail.com>
2024-01-08 12:29:17 +0530

    Simple array program is added

---
+++ SimpleArray.php
@@ -0,0 +1,10 @@
+<?php
+
+    $arr =  array("BMW","volvo","audi","tata","ford","mahindra","toyota");
+
+    foreach($arr as $x){
+        echo "Cars company :$x<br>";
+    }
+    echo "This is simple example of array in PHP <br>";
+
+?>
