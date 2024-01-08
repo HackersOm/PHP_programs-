87dae08fc336054d0e353f36e6810c774186ab88
hacker <hackeromsutar@gmail.com>
2024-01-08 12:11:00 +0530

    hello php is add

---
+++ hello.php
@@ -0,0 +1,22 @@
+<?php
+  $a = 10;
+  $b = true;
+  $c = "Hello this is string ";
+  $d = 10.666;
+  $e = array ("this ","is ","array");
+  $f = null;
+
+  var_dump($a);
+  echo "<br>";
+  var_dump($b);
+  echo "<br>";
+  var_dump($c);
+  echo "<br>";
+  var_dump($d);
+  echo "<br>";
+  var_dump($e);
+  echo "<br>";
+  var_dump($f);
+
+  echo "<br>This is working ";
+ ?>
