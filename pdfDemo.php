5d5743120cd232abfdf9e50b3eefdbcf0a05119f
HackersOm <hackeromsutar@gmail.com>
2024-02-09 13:03:30 +0530

    Change in pdfDemo and added image in it

--- pdfDemo.php
+++ pdfDemo.php
@@ -3,15 +3,7 @@
     $pdf = new FPDF();
     $pdf->AddPage();
     $pdf->SetFont('Arial','B',20);
-    $pdf->Cell(150,150,"This is working ",1,0,'C');
+    $pdf->Cell(40,40,"This is working ");
+    $pdf->Image("Dragon.jpg",30,40);
     $pdf->Output();
-
-   
-// require('fpdf.php');
-
-// $pdf = new FPDF();
-// $pdf->AddPage();
-// $pdf->SetFont('Arial','B',16);
-// $pdf->Cell(40,10,'Hello World !');
-// $pdf->Output();
 ?>  
